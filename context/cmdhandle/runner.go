593ade7f023a58ea32d6bcdda9ba583d29b69791
Thomas Ziegler <t.ziegler@bigpoint.net>
2021-03-04 10:58:39 +0100

    added bash/zsh/fish autocompletion by cobra

--- context/cmdhandle/runner.go
+++ context/cmdhandle/runner.go
@@ -48,6 +48,56 @@ if you enter or leave them.`,
 		},
 	}
 
+	completionCmd = &cobra.Command{
+		Use:   "completion [bash|zsh|fish|powershell]",
+		Short: "Generate completion script",
+		Long: `To load completions:
+
+Bash:
+
+  $ source <(contxt completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ yourprogram completion bash > /etc/bash_completion.d/yourprogram
+  # macOS:
+  $ yourprogram completion bash > /usr/local/etc/bash_completion.d/yourprogram
+
+Zsh:
+
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
+
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ yourprogram completion zsh > "${fpath[1]}/_yourprogram"
+
+  # You will need to start a new shell for this setup to take effect.
+
+fish:
+
+  $ yourprogram completion fish | source
+
+  # To load completions for each session, execute once:
+  $ yourprogram completion fish > ~/.config/fish/completions/yourprogram.fish
+
+  `,
+		DisableFlagsInUseLine: true,
+		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+		Args:                  cobra.ExactValidArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				cmd.Root().GenZshCompletion(os.Stdout)
+			case "fish":
+				cmd.Root().GenFishCompletion(os.Stdout, true)
+			}
+		},
+	}
+
 	dirCmd = &cobra.Command{
 		Use:   "dir",
 		Short: "handle workspaces and assigned paths",
@@ -303,6 +353,8 @@ func initCobra() {
 	lintCmd.Flags().Bool("parse", false, "parse second level keywords (#@...)")
 
 	rootCmd.AddCommand(lintCmd)
+
+	rootCmd.AddCommand(completionCmd)
 
 }
 
