e06f1dfa5fe4c7a2de496e02a1cfbb76753075c4
Thomas Ziegler <thomas.zglr@googlemail.com>
2022-07-29 15:17:12 +0200

    rework of ui

--- context/cmdhandle/interactive.go
+++ context/cmdhandle/interactive.go
@@ -3,11 +3,11 @@ package cmdhandle
 import (
 	"fmt"
 	"strconv"
-	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
+	"github.com/swaros/contxt/context/configure"
 )
 
 type CtxUi struct {
@@ -35,11 +35,23 @@ func InitWindow(cmd *cobra.Command, args []string) (*CtxUi, error) {
 	menu.SetBorder(true)
 	menu.SetBackgroundColor(tcell.ColorDarkBlue)
 	menu.SetMainTextColor(tcell.ColorLightBlue)
+
+	status := tview.NewTextView()
+	status.SetText("[blue]version [yellow]" + configure.GetVersion() + "\n [blue]build[yellow] " + configure.GetBuild())
+	status.SetBorder(true)
+	status.SetDynamicColors(true)
+
+	mainWindow := tview.NewTextView()
+	mainWindow.SetBorder(true)
+
 	flex := tview.NewFlex().
-		AddItem(menu, 0, 1, true)
+		AddItem(menu, 0, 1, true).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(status, 0, 1, false).
+			AddItem(mainWindow, 0, 5, false), 0, 3, false)
 
 	pages.AddPage("main", flex, true, true)
-	app.SetRoot(pages, true)
+	app.SetRoot(pages, true).EnableMouse(false)
 
 	if err := app.Run(); err != nil {
 		return ui, err
@@ -59,15 +71,6 @@ func (ui *CtxUi) createMenu() *tview.List {
 	return menu
 }
 
-func (ui *CtxUi) updateTime() {
-	for {
-		time.Sleep(time.Millisecond * 500)
-		ui.app.QueueUpdateDraw(func() {
-			ui.outscr.SetText(ui.LogOutMessage)
-		})
-	}
-}
-
 func (ui *CtxUi) CreateTargetList() *tview.Flex {
 	// this list contains ans target and we use them as a menu
 	list := tview.NewList()
@@ -76,9 +79,12 @@ func (ui *CtxUi) CreateTargetList() *tview.Flex {
 			list.AddItem(target, "run target "+target, rune(strconv.Itoa(index)[0]), nil)
 		}
 	}
-
+	list.SetHighlightFullLine(true)
 	list.SetSelectedFunc(func(i int, target, s2 string, r rune) {
 		if r != 'q' {
+			if ui.outscr != nil {
+				ui.outscr.Clear()
+			}
 			go RunTargets(target, true)
 		}
 	})
@@ -99,20 +105,12 @@ func (ui *CtxUi) CreateTargetList() *tview.Flex {
 	PreHook = func(msg ...interface{}) bool {
 		byteData := []byte(tview.TranslateANSI(fmt.Sprintln(msg...)))
 		output.Write(byteData)
-
-		//output.SetText(output.GetText(false) + tview.TranslateANSI(fmt.Sprint(msg...)))
-		//ui.app.QueueUpdateDraw(func() {})
-		/*
-			go func(app *tview.Application) {
-				app.Draw()
-			}(ui.app)
-		*/
 		return true
 	}
 	CtxOut("running target")
 
 	targetflex := tview.NewFlex().
-		AddItem(list, 0, 1, true).AddItem(output, 0, 1, false)
+		AddItem(list, 0, 1, true).AddItem(output, 0, 4, false)
 	return targetflex
 
 }
