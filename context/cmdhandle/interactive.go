6180968e2f26781732202dd7522409a17ff6bdf6
Thomas Ziegler <thomas.zglr@googlemail.com>
2022-07-29 19:28:09 +0200

    update interactive workflow

--- context/cmdhandle/interactive.go
+++ context/cmdhandle/interactive.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 	"github.com/swaros/contxt/context/configure"
+	"github.com/swaros/manout"
 )
 
 type CtxUi struct {
@@ -19,6 +20,8 @@ type CtxUi struct {
 	outscr        *tview.TextView
 	args          []string
 	LogOutMessage string
+	mainScr       *tview.TextView
+	statusScr     *tview.TextView
 }
 
 func InitWindow(cmd *cobra.Command, args []string) (*CtxUi, error) {
@@ -40,9 +43,14 @@ func InitWindow(cmd *cobra.Command, args []string) (*CtxUi, error) {
 	status.SetText("[blue]version [yellow]" + configure.GetVersion() + "\n [blue]build[yellow] " + configure.GetBuild())
 	status.SetBorder(true)
 	status.SetDynamicColors(true)
+	ui.statusScr = status
 
 	mainWindow := tview.NewTextView()
-	mainWindow.SetBorder(true)
+	mainWindow.SetBorder(true).
+		SetBackgroundColor(tcell.ColorDarkGoldenrod)
+	mainWindow.SetDynamicColors(true)
+
+	ui.mainScr = mainWindow
 
 	flex := tview.NewFlex().
 		AddItem(menu, 0, 1, true).
@@ -53,26 +61,31 @@ func InitWindow(cmd *cobra.Command, args []string) (*CtxUi, error) {
 	pages.AddPage("main", flex, true, true)
 	app.SetRoot(pages, true).EnableMouse(false)
 
+	ui.startCapture()
 	if err := app.Run(); err != nil {
 		return ui, err
 	}
+
 	return ui, nil
 }
 
 func (ui *CtxUi) createMenu() *tview.List {
 
-	menu := tview.NewList().AddItem("tasks", "run tasks", 'r', func() {
+	menu := tview.NewList().AddItem("tasks", "start Task", 'r', func() {
 		ui.pages.SendToFront("target")
 	}).AddItem("quit", "exit interactive mode", 'q', func() {
 		ui.app.Stop()
 	})
-	ui.pages.AddPage("target", ui.CreateTargetList(), true, true)
+	ui.pages.AddPage("target", ui.CreateRunPage(), true, true)
 	ui.menu = menu
 	return menu
 }
 
-func (ui *CtxUi) FilterOutPut(msg ...interface{}) []interface{} {
-	var newMsh []interface{}
+// FilterOutPut parses the content and handles
+// all interface depending the Type differently
+func (ui *CtxUi) FilterOutPut(caseHandle func(target string, msg []interface{}), msg ...interface{}) []interface{} {
+	var newMsh []interface{} // new hash for the output
+	haveTarget := ""
 	for _, chk := range msg {
 		switch chk.(type) {
 		case CtxOutCtrl:
@@ -80,18 +93,58 @@ func (ui *CtxUi) FilterOutPut(msg ...interface{}) []interface{} {
 				return newMsh
 			}
 			continue
+		case CtxOutLabel:
+			ctrl := chk.(CtxOutLabel)
+			newMsh = append(newMsh, manout.Message(ctrl.Message))
+			continue
 		case CtxTargetOut:
 			ctrl := chk.(CtxTargetOut)
+			haveTarget = ctrl.Target
 			newMsh = append(newMsh, ctrl.Target)
 		default:
 			newMsh = append(newMsh, chk)
 		}
 
 	}
+	if haveTarget != "" {
+		caseHandle(haveTarget, newMsh)
+		var dwMsh []interface{}
+		return dwMsh
+	}
 	return newMsh
 }
 
-func (ui *CtxUi) CreateTargetList() *tview.Flex {
+// startCapture set up the output capturing.
+// it is also the method that is the "tick"
+// because it will be triggered on statusmessage
+// So this is the place to update all components
+func (ui *CtxUi) startCapture() {
+	// we set the PreHook so any Message that is send to
+	// CtxOut will be handled from now on by this function
+	PreHook = func(msg ...interface{}) bool {
+		msg = ui.FilterOutPut(func(target string, msg []interface{}) {
+			if ui.outscr != nil {
+				byte4main := []byte(tview.TranslateANSI(fmt.Sprintln(msg...)))
+				ui.outscr.Write(byte4main)
+			}
+		}, msg...) // filter output depending types of the content
+
+		if len(msg) > 0 && ui.mainScr != nil {
+			byteData := []byte(tview.TranslateANSI(fmt.Sprintln(msg...)))
+			ui.mainScr.Write(byteData)
+
+			if ui.outscr != nil {
+				byte4main := []byte(tview.TranslateANSI(fmt.Sprintln(msg...)))
+				ui.outscr.Write(byte4main)
+			}
+		}
+
+		return true
+	}
+	CtxOut("running target")
+}
+
+func (ui *CtxUi) CreateRunPage() *tview.Flex {
 	// this list contains ans target and we use them as a menu
 	list := tview.NewList()
 	if targets, ok := getAllTargets(); ok {
@@ -121,16 +174,7 @@ func (ui *CtxUi) CreateTargetList() *tview.Flex {
 			ui.app.Draw()
 		})
 	output.SetBorder(true).SetTitle("log")
-	// we set the PreHook so any Message that is send to
-	// CtxOut will be handled from now on by this function
-	PreHook = func(msg ...interface{}) bool {
-		msg = ui.FilterOutPut(msg...) // filter output depending types of the content
-		byteData := []byte(tview.TranslateANSI(fmt.Sprintln(msg...)))
-		output.Write(byteData)
-		return true
-	}
-	CtxOut("running target")
-
+	ui.outscr = output
 	targetflex := tview.NewFlex().
 		AddItem(list, 0, 1, true).AddItem(output, 0, 4, false)
 	return targetflex
