7de7a550783bec8f21ba9285c17fb609efaf944a
Thomas Ziegler <thomas.zglr@googlemail.com>
2022-08-16 08:40:12 +0200

    code cleanup 2

--- module/configure/version_test.go
+++ module/configure/version_test.go
@@ -1,6 +1,8 @@
 package configure_test
 
 import (
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/swaros/contxt/configure"
@@ -17,5 +19,22 @@ func TestVersion(t *testing.T) {
 
 	if configure.CheckVersion("0.3", "0.2.1") == true {
 		t.Error("did we reach version 0.3 already? ", configure.GetVersion())
+	}
+}
+
+func TestDefaultValues(t *testing.T) {
+	if configure.GetBuild() != "" {
+		t.Error("unexpected default value")
+	}
+
+	if configure.GetVersion() != "" {
+		t.Error("unexpected default value")
+	}
+
+	// This is the most useless test i wrote in my life.
+	// but i could not resist just because of the red color
+	// in the coverage
+	if configure.GetOs() != strings.ToLower(runtime.GOOS) {
+		t.Error("what the .... how could this fail?")
 	}
 }
