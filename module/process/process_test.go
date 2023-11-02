c4abfad7d6b87d19d77eab96cdb590e3c929aecf
Thomas Ziegler <tziegler@bigpoint.net>
2023-11-02 15:49:53 +0100

    fix some runtime handling issues

--- module/process/process_test.go
+++ module/process/process_test.go
@@ -17,6 +17,29 @@ func TestBasicRun(t *testing.T) {
 
 }
 
+func TestBasicRunButCommand(t *testing.T) {
+	process := process.NewProcess("bash", "-c", "echo 'Hello World'")
+
+	if err := process.Command("echo 'Hello World 2'"); err == nil {
+		t.Error("Error is nil")
+	} else {
+		if err.Error() != "process is not started" {
+			t.Error("Error is not 'process is not started'. It is ", err.Error())
+		}
+	}
+	if _, _, err := process.Exec(); err != nil {
+		t.Error(err)
+	}
+
+	if err := process.Command("echo 'Hello World 2'"); err == nil {
+		t.Error("Error is nil")
+	} else {
+		if err.Error() != "process is not set to stay open" {
+			t.Error("Error is not 'process is not set to stay open'. It is ", err.Error())
+		}
+	}
+}
+
 func TestBasicRunWithError(t *testing.T) {
 	process := process.NewProcess("notExists")
 	if inCode, realCode, err := process.Exec(); err == nil {
@@ -51,8 +74,6 @@ func TestExecWithBash(t *testing.T) {
 	process.SetOnInit(func(proc *os.Process) {
 		if proc == nil {
 			t.Error("Process is nil")
-		} else {
-			t.Logf("Process started with pid %d", proc.Pid)
 		}
 	})
 	realCode, internCode, err := process.Exec()
@@ -119,6 +140,7 @@ func TestExecWithBashAndStayOpenAndError(t *testing.T) {
 	proc := process.NewProcess("bash")
 	proc.SetKeepRunning(true)
 	proc.SetOnOutput(func(msg string, err error) bool {
+		t.Log("MESSAGE:[", msg, "]")
 		if err != nil {
 			errors = append(errors, err)
 			return false
@@ -130,27 +152,40 @@ func TestExecWithBashAndStayOpenAndError(t *testing.T) {
 	proc.SetOnInit(func(proc *os.Process) {
 		if proc == nil {
 			t.Error("Process is nil")
-		} else {
-			t.Logf("Process started with pid %d", proc.Pid)
 		}
 	})
 
-	proc.Exec()
+	if _, _, err := proc.Exec(); err != nil {
+		t.Error(err)
+		t.SkipNow()
+	}
 
-	proc.Command("echo 'Hello World'")
-	proc.Command("notACommand")
-	// give the process some time to execute the command
-	time.Sleep(100 * time.Millisecond)
-	realCode, internCode, err := proc.Stop()
-
-	if err != nil {
+	if err := proc.Command("echo 'Hello World'"); err != nil {
 		t.Error(err)
 	}
-	if realCode != 0 {
-		t.Error("realCode is not 0. It is ", realCode)
+
+	if err := proc.Command("notACommand"); err != nil {
+		t.Error(err)
 	}
-	if internCode != 0 {
-		t.Error("internCode is not 0, It is ", internCode)
+	// give the process some time to execute the command
+	time.Sleep(100 * time.Millisecond)
+	internCode, realCode, err := proc.Stop()
+
+	// this should fail because the command is not found
+	// so bash will exit with 127
+	if err != nil {
+		// expected error is "exit status 127"
+		if err.Error() != "exit status 127" {
+			t.Error("unexpected error: ", err)
+		}
+	} else {
+		t.Error("expected error but got none")
+	}
+	if realCode != 127 {
+		t.Error("realCode is not 127. It is ", realCode)
+	}
+	if internCode != 103 {
+		t.Error("internCode is not 103, It is ", internCode)
 	}
 
 	if len(outPuts) != 2 {
@@ -168,4 +203,40 @@ func TestTimeOut(t *testing.T) {
 	proc.SetKeepRunning(true)
 	proc.SetTimeout(100 * time.Millisecond)
 
+	if _, _, err := proc.Exec(); err != nil {
+		t.Error(err)
+		t.SkipNow()
+	}
+	messureStartTimeout := time.Now()
+	if err := proc.Command("sleep 10"); err != nil {
+		t.Error(err)
+	}
+	// using BlockWait to wait for the process to stop
+	if err := proc.BlockWait(10 * time.Millisecond); err != nil {
+		t.Error(err)
+	}
+	internCode, realCode, err := proc.Stop()
+	expectedInternCode := process.ExitTimeout
+	expectedRealCode := process.RealCodeNotPresent
+
+	// check the needed time. it should be around 100ms
+	// but we give it 300ms to be sure
+	if time.Since(messureStartTimeout) > 300*time.Millisecond {
+		t.Error("timeout is not working")
+	}
+
+	if err != nil {
+		if err.Error() != "process stopped by timeout" {
+			t.Error("unexpected error: ", err)
+		}
+	} else {
+		t.Error("expected error but got none")
+	}
+
+	if realCode != expectedRealCode {
+		t.Error("realCode is not ", expectedRealCode, ". It is ", realCode)
+	}
+	if internCode != expectedInternCode {
+		t.Error("internCode is not ", expectedInternCode, ", It is ", internCode)
+	}
 }
--- module/process/wrapp.go
+++ module/process/wrapp.go
@@ -1,6 +1,9 @@
 package process
 
-import "os"
+import (
+	"os"
+	"os/exec"
+)
 
 // basic struct to hold the data of a process
 type ProcData struct {
@@ -10,7 +13,7 @@ type ProcData struct {
 	Threads     []int  // list of threads pids
 }
 
-type ProcessDef struct {
+type ProcessWatch struct {
 	pData       *ProcData
 	processInfo *os.Process
 }
@@ -21,16 +24,67 @@ func NewProc(pid int) (*ProcData, error) {
 	return ReadProc(pid)
 }
 
-// NewProcessWatcher creates a new ProcessDef struct
+// NewProcessWatcherByCmd creates a new ProcessDef struct
 // and returns a pointer to it
 // the ProcessDef struct holds the process data of the given process
-func NewProcessWatcher(proc *os.Process) (*ProcessDef, error) {
+func NewProcessWatcherByCmd(cmd *exec.Cmd) (*ProcessWatch, error) {
+	if pdef, err := NewProc(cmd.Process.Pid); err != nil {
+		return nil, err
+	} else {
+		return &ProcessWatch{
+			pData:       pdef,
+			processInfo: cmd.Process,
+		}, nil
+	}
+}
+
+func NewProcessWatcherByProcessInfo(proc *os.Process) (*ProcessWatch, error) {
 	if pdef, err := NewProc(proc.Pid); err != nil {
 		return nil, err
 	} else {
-		return &ProcessDef{
+		return &ProcessWatch{
 			pData:       pdef,
 			processInfo: proc,
 		}, nil
 	}
+}
+
+func (proc *ProcessWatch) GetPid() int {
+	return proc.pData.Pid
+}
+
+func (proc *ProcessWatch) GetCmd() string {
+	return proc.pData.Cmd
+}
+
+func (proc *ProcessWatch) GetThreadCount() int {
+	return proc.pData.ThreadCount
+}
+
+func (proc *ProcessWatch) GetThreads() []int {
+	return proc.pData.Threads
+}
+
+func (proc *ProcessWatch) GetProcessInfo() *os.Process {
+	return proc.processInfo
+}
+
+func (proc *ProcessWatch) Kill() error {
+	return KillProcessTree(proc.pData.Pid)
+}
+
+func (proc *ProcessWatch) Update() error {
+	if pdef, err := NewProc(proc.pData.Pid); err != nil {
+		return err
+	} else {
+		proc.pData = pdef
+		return nil
+	}
+}
+
+func (proc *ProcessWatch) IsRunning() (bool, error) {
+	if err := proc.Update(); err != nil {
+		return false, err
+	}
+	return proc.pData.Pid > 0, nil
 }
