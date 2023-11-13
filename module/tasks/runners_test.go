dea1385334b9f8c6c3a70ea536e228fba2836fe4
Thomas Ziegler <tziegler@bigpoint.net>
2023-11-13 14:57:48 +0100

    add runnerCtrl

--- module/tasks/runners_test.go
+++ module/tasks/runners_test.go
@@ -18,9 +18,7 @@ func TestRunnerBasic(t *testing.T) {
 	task := tasks.New(testTask.ID, nil)
 
 	messages := []string{}
-	//weGotSomething := make(chan bool)
 	runner, err := task.GetRunnerForTask(testTask, func(s string, err error) bool {
-		//weGotSomething <- true
 		if err != nil {
 			t.Error(err)
 		}
@@ -33,7 +31,7 @@ func TestRunnerBasic(t *testing.T) {
 	if runner == nil {
 		t.Error("runner is nil")
 	} else {
-		if err := runner.Command("ls -ga"); err != nil {
+		if err := runner.GetRunner().Command("ls -ga"); err != nil {
 			t.Error(err)
 		} else {
 			// wait to the task get running have to be very fast
@@ -41,11 +39,9 @@ func TestRunnerBasic(t *testing.T) {
 			// so this test can fail if the system is faster then the system was using while writing the test
 			// and 10 nanoseconds is then to long to wait.
 			// so we will ignore the false result for testing
-			task.WaitTilTaskRunnerIsRunning(testTask, 10*time.Nanosecond, 100) // so first make sure the task is running
-
-			time.Sleep(50 * time.Millisecond)
-			task.WaitTilTaskRunnerIsDone(testTask, 10*time.Millisecond) // and then wait til it is done
-			if task.RunnersActive() {                                   // this shuld be obvious but we check it anyway
+			task.WaitTilTaskRunnerIsRunning(testTask, 1*time.Millisecond, 100) // so first make sure the task is running. at least wait 100 millisecond
+			task.WaitTilTaskRunnerIsDone(testTask, 10*time.Millisecond)        // and then wait til it is done
+			if task.RunnersActive() {                                          // this shuld be obvious but we check it anyway
 				t.Error("runner should not be active anymore")
 			}
 			// messages should contains the names of the files in the current directory
@@ -73,4 +69,44 @@ func TestRepeatedBasic(t *testing.T) {
 	for i := 0; i < countOfTest; i++ {
 		TestRunnerBasic(t)
 	}
+}
+
+func TestRunnerCmd(t *testing.T) {
+
+	var testTask configure.Task = configure.Task{
+		ID: "testsleep", // we need an ID for the task
+	}
+	task := tasks.New(testTask.ID, nil)
+	messages := []string{}
+	runner, err := task.GetRunnerForTask(testTask, func(s string, err error) bool {
+		if err != nil {
+			t.Error(err)
+		}
+		messages = append(messages, s)
+		return true
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if runner == nil {
+		t.Error("runner is nil")
+	} else {
+		if err := runner.Cmd("ls -ga"); err != nil {
+			t.Error(err)
+		} else {
+			if len(messages) == 0 {
+				t.Error("no messages received")
+			} else {
+				expected := []string{"runners_test.go", "..", ".", "watchman.go", "watchmanTask.go"}
+				for _, e := range expected {
+					if !systools.SliceContainsSub(messages, e) {
+						t.Error("missing", e, "in messages:", strings.Join(messages, "\n"))
+					}
+				}
+			}
+			task.StopAllTaskRunner() // at least stop the task runner
+
+		}
+	}
+
 }
