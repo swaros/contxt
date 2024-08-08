995af0708a5a2e5ccf49243a6df36165985c0fba
Thomas Ziegler <tziegler@bigpoint.net>
2024-08-08 16:30:56 +0200

    Update deps, buildinfo and licence

--- winbuild.bat
+++ winbuild.bat
@@ -1,4 +1,4 @@
 echo "build script"
 echo "-> build release"
-go build -ldflags " -X github.com/swaros/contxt/module/configure.minversion=3 -X github.com/swaros/contxt/module/configure.midversion=5 -X github.com/swaros/contxt/module/configure.mainversion=0 -X github.com/swaros/contxt/module/configure.build=.20231020.130226-linux-release" -o ./bin/contxt.exe cmd/cmd-contxt/main.go
+go build -ldflags " -X github.com/swaros/contxt/module/configure.minversion=3 -X github.com/swaros/contxt/module/configure.midversion=5 -X github.com/swaros/contxt/module/configure.mainversion=0 -X github.com/swaros/contxt/module/configure.build=.20240808.142542-linux-release" -o ./bin/contxt.exe cmd/cmd-contxt/main.go
 echo "-> build release done"
