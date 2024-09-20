2679bf890180597b1311690797bc82f3b91fa561
Thomas Ziegler <tziegler@bigpoint.net>
2024-09-20 08:04:17 +0200

    full buildata update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "build script"
 ### build release
 
 # compose build ldflags
-BUILD_ARGS=" -X github.com/swaros/contxt/module/configure.minversion=3 -X github.com/swaros/contxt/module/configure.midversion=5 -X github.com/swaros/contxt/module/configure.mainversion=0 -X github.com/swaros/contxt/module/configure.build=.20240808.142542-linux-release"
+BUILD_ARGS=" -X github.com/swaros/contxt/module/configure.minversion=3 -X github.com/swaros/contxt/module/configure.midversion=5 -X github.com/swaros/contxt/module/configure.mainversion=0 -X github.com/swaros/contxt/module/configure.build=.20240920.060144-linux-release"
 # build release
 echo "-> build release"
 go build -ldflags "$BUILD_ARGS" -o ./bin/contxt cmd/cmd-contxt/main.go
