15e6086a5ddd42a85bf4e758f71273c63395c281
Thomas Ziegler <tziegler@bigpoint.net>
2024-09-23 14:57:27 +0200

    fix create import test depending keyedFolders

--- module/runner/create_test.go
+++ module/runner/create_test.go
@@ -22,6 +22,7 @@ func TestCreateImport(t *testing.T) {
   basedir: false
   folders:
   - testdata/task2/.contxt.yml
+  keyedFolders: []
 `
 	if added != expected {
 		t.Errorf("Expected \n%s\ngot\n%s", expected, added)
@@ -47,6 +48,7 @@ func TestCreateImport(t *testing.T) {
   folders:
   - testdata/task2/.contxt.yml
   - testdata/task3/.contxt.yml
+  keyedFolders: []
 `
 
 	if added != expected {
